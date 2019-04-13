79485c21522d421c212ed3055936232ae899e896
AnikaTabassum <35559892+AnikaTabassum@users.noreply.github.com>
2019-04-13 22:54:30 +0600

    final db for objects

--- Demo/database.py
+++ Demo/database.py
@@ -1,7 +1,18 @@
-create database logindb;
-create table itemss(
-	name varchar(80) not null,
-	imageName longblob not null,
-	videoName longblob not null,
-	audioName longblob not null);
-select name,substring(imageName,1,20),substring(videoName,1,20),substring(audioName,1,20) from itemss;
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = 'localhost',
+    user = "root",
+    #passwd = "",
+    database = "spl"
+    #auth_plugin='mysql_native_password'
+    )
+myCursor = mydb.cursor()
+#myCursor.execute("CREATE database test222")
+sql="CREATE TABLE object (object_id int auto_increment PRIMARY KEY, image_name_1 VARCHAR(200), image_name_2 VARCHAR(200),image_name_3 VARCHAR(200),\
+video_name VARCHAR(200), object_image VARCHAR(200), audio_name VARCHAR(200), object_name VARCHAR(200))"
+myCursor.execute(sql)
+mydb.commit()
+myCursor.close()
+mydb.close()
+
