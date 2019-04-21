24a3a69e094b701739c024a3ee4d134b69313602
A. N. TANVIR <35560149+antanvir@users.noreply.github.com>
2019-04-22 01:44:19 +0600

    Add files via upload

--- Final Codes/StartingPage.py
+++ Final Codes/StartingPage.py
@@ -31,64 +31,30 @@ class StartingPage(QWidget):
         horUnit = int(self.width / 12)
         verUnit = int(self.height / 12)
 
-        '''
-        space = "       "
-        learnTitle = "\n" + space + "Learn Different Objects" + space + "\n"
-        gameTitle = "\n" + space + "Play Puzzle Games" + space + "\n"
-        developmentTitle = "\n" + space + "Show My Development" + space + "\n"
-                '''
         self.label = QLabel(self)
         self.label.setText(" CHOOSE  ANY  MODULE \n___________________________")
-        self.label.setGeometry(2*horUnit, 0.5*verUnit, 8*horUnit, 1.5*verUnit)
+        self.label.setGeometry(3*horUnit, 0.5*verUnit, 6*horUnit, 1.5*verUnit)
         self.label.setStyleSheet("color: white; font-size: 35px; font-weight: bold;")
         self.label.setAlignment(Qt.AlignCenter)
 
-        #layout = QVBoxLayout()
-        # layout.addStretch()
+
         self.btn1 = QPushButton("LEARN  DIFFERENT  OBJECTS", self)
-        #self.btn1.resize(500, 50)
-        #self.btn1.move(350, 100)
-        self.btn1.setGeometry(2*horUnit, 3*verUnit, 8*horUnit, 1*verUnit)
+        self.btn1.setGeometry(3*horUnit, 3*verUnit, 6*horUnit, 1*verUnit)
         self.btn1.setStyleSheet("background-color: white; font-weight: bold; font-size: 25px; color: green;")
         self.btn1.clicked.connect(self.btn_1_clicked)
 
-        # layout.addWidget(self.btn1)
-        # layout.addStretch()
 
         self.btn2 = QPushButton("PLAY  PUZZLE  GAMES", self)
-        #self.btn2.resize(500, 50)
-        #self.btn2.move(350, 250)
-        self.btn2.setGeometry(2*horUnit, 5.5*verUnit, 8*horUnit, 1*verUnit)
+        self.btn2.setGeometry(3*horUnit, 5.5*verUnit, 6*horUnit, 1*verUnit)
         self.btn2.setStyleSheet("background-color: white; font-weight: bold; font-size: 25px; color: green;")
-        # self.b2.setIcon(QIcon(QPixmap("python.gif")))
         self.btn2.clicked.connect(self.btn_2_clicked)
-        # layout.addWidget(self.btn2)
-        # layout.addStretch()
+
 
         self.btn3 = QPushButton("SHOW  MY  DEVELOPMENT", self)
-        # self.b3.setEnabled(False)
-        #self.btn3.resize(500, 50)
-        #self.btn3.move(350, 400)
-        self.btn3.setGeometry(2*horUnit, 8*verUnit, 8*horUnit, 1*verUnit)
+        self.btn3.setGeometry(3*horUnit, 8*verUnit, 6*horUnit, 1*verUnit)
         self.btn3.setStyleSheet("background-color: white; font-weight: bold; font-size: 25px; color: green;")
         self.btn3.clicked.connect(self.btn_3_clicked)
-        # layout.addWidget(self.btn3)
-        # layout.addStretch()
 
-        '''
-        hbox = QHBoxLayout()
-        hbox.addStretch()
-        hbox.addLayout(layout)
-        hbox.addStretch()
-		'''
-        # self.setLayout(hbox)
-        '''
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        self.setStyleSheet("background-color: lightgray;")
-        '''
-        # self.show()
-        # self.setWindowTitle("WelcomeS")
 
     def btn_1_clicked(self):
         self.learn = LearningModule()
